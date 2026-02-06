e624f8e8053cd47ca2d8eb2febfefde5a4faabe6
Ibrahim <ibrahimsakariyah@gmail.com>
2026-02-06 17:32:09 +0100

    erase overlap intervals solved

---
+++ blind75/non_overlapping_interval.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # step 1: Sort by end time (this sort by 2nd element in the array [start, end])
+        intervals.sort(key=lambda x: x[1])
+
+        count_removed = 0
+        prev_end = intervals[0][1]
+
+
+        # step 2:  iterate through the first intervals
+        for start, end in intervals[1:]:
+            if start < prev_end:
+                # Overlap - remove this interval
+                count_removed +=1
+            else:
+                # No Overlap - keep it
+                prev_end = end
+
+        return count_removed
+
+
+# how the lambda function works in python in thr greedy algorithm
+# it is used to Order intervals so the one that finishes earliest comes first, so greedy can work.
+
+# def get_end_time(x): # x = [start, end]
+#     return x[1] # x[1] = end
+# therefore, the sort is sorting the list using the key, which is returning the end time in order of the smallest value.
